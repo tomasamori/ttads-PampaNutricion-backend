b2e2b71625f404b4b4f631f7089845d010e45e03
Tomás Amori <tomasamori15@gmail.com>
2022-07-11 23:34:58 -0300

    se agrega backend de Sucursales

--- src/routes/api/v1/index.js
+++ src/routes/api/v1/index.js
@@ -1,5 +1,8 @@
-import express from 'express';
+import express from "express";
+import sucursal from "./sucursal";
 
 const router = express.Router();
+
+router.use('/sucursal', sucursal);
 
 export default router;
---
+++ src/routes/api/v1/sucursal.js
@@ -0,0 +1,54 @@
+import express from "express";
+import Sucursal from "../../../models/Sucursal";
+
+const router = express.Router();
+
+// curl -UseBasicParsing -Method GET -URI http://localhost:3000/api/v1/sucursal/
+// getAll
+
+router.get('/', async(req, res) => {
+    let sucursales = await Sucursal.find();
+    return res.status(200).json({
+        success: true,
+        data: sucursales,
+        message: "Lista de sucursales recuperada exitosamente"
+    })
+});
+
+// curl -UseBasicParsing -Method GET -URI http://localhost:3000/api/v1/sucursal/<id>
+// getOne
+
+router.get('/:id', async(req, res) => {
+    let suc = await Sucursal.findOne({"_id": req.params.id});
+    return res.status(200).json({
+        success:true,
+        data: suc,
+        message: "Sucursal encontrada"
+    })
+});
+
+// curl -UseBasicParsing -Method POST -Headers @{"Content-Type"= "application/json"} -Body '{"nombre": "test", "direccion": "test"}' -URI http://localhost:3000/api/v1/sucursal
+// newSucursal
+
+router.post('/', async(req, res) => {
+    let suc = await new Sucursal(req.body);
+    await suc.save();
+    return res.status(200).json({
+        success: true,
+        data: suc,
+        message: 'Sucursal agregada exitosamente'
+    })
+});
+
+// curl -UseBasicParsing -Method DELETE -URI http://localhost:3000/api/v1/sucursal/<id>
+
+router.delete('/:id', async(req, res) => {
+    await Sucursal.deleteOne({"_id": req.params.id});
+    return res.status(200).json({
+        success: true,
+        data: {"_id": req.params.id},
+        message: 'Sucursal eliminada exitosamente'
+    })
+});
+
+export default router;
