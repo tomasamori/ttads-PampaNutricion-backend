2aebc1e05229baa60acd5ed219714c77c28e34fd
Tomás Amori <tomasamori15@gmail.com>
2022-07-12 10:59:16 -0300

    se agrega update de Sucursal y Producto

--- src/routes/api/v1/producto.js
+++ src/routes/api/v1/producto.js
@@ -27,20 +27,34 @@ router.get('/:id', async(req,res) => {
     })
 });
 
-//curl -UseBasicParsing -Method POST -Headers @{"Content-Type"="application/json"} -Body '{"marca":"test", "nombre":"test", "descripcion":"test", "peso":"test"}' -URI http://localhost:3000/api/v1/producto
+// curl -UseBasicParsing -Method POST -Headers @{"Content-Type"="application/json"} -Body '{"marca":"test", "nombre":"test", "descripcion":"test", "peso":"test"}' -URI http://localhost:3000/api/v1/producto
 // newProducto
 
 router.post('/', async(req, res)=>{
     let pro = await new Producto(req.body);
     await pro.save();
-    return res.status(200).json({
+    return res.status(201).json({
         success: true,
         data: pro,
         message: 'Producto agregado exitosamente'
     })
 });
 
-//curl -UseBasicParsing -Method DELETE -URI http://localhost:3000/api/v1/producto/<id>
+// curl -UseBasicParsing -Method PUT -Headers @{"Content-Type"= "application/json"} -Body '{"marca":"cambio", "nombre":"cambio", "descripcion":"cambio", "peso":"cambio"}' -URI http://localhost:3000/api/v1/producto/<id>
+// updateProducto
+
+router.put('/:id', async(req, res) => {
+    let pro = await Producto.findOne({"_id": req.params.id});
+    await pro.update(req.body);
+    return res.status(200).json({
+        success: true,
+        data: {"_id": req.params.id},
+        message: 'Producto actualizado exitosamente'
+    })
+});
+
+// curl -UseBasicParsing -Method DELETE -URI http://localhost:3000/api/v1/producto/<id>
+// deleteProducto
 
 router.delete('/:id', async(req, res)=> {
     await Producto.deleteOne({"_id":req.params.id});
--- src/routes/api/v1/sucursal.js
+++ src/routes/api/v1/sucursal.js
@@ -33,14 +33,28 @@ router.get('/:id', async(req, res) => {
 router.post('/', async(req, res) => {
     let suc = await new Sucursal(req.body);
     await suc.save();
-    return res.status(200).json({
+    return res.status(201).json({
         success: true,
         data: suc,
         message: 'Sucursal agregada exitosamente'
     })
 });
 
+// curl -UseBasicParsing -Method PUT -Headers @{"Content-Type"= "application/json"} -Body '{"nombre": "cambio", "direccion": "cambio"}' -URI http://localhost:3000/api/v1/sucursal/<id>
+// updateSucursal
+
+router.put('/:id', async(req, res) => {
+    let suc = await Sucursal.findOne({"_id": req.params.id});
+    await suc.update(req.body);
+    return res.status(200).json({
+        success: true,
+        data: {"_id": req.params.id},
+        message: 'Sucursal actualizada exitosamente'
+    })
+});
+
 // curl -UseBasicParsing -Method DELETE -URI http://localhost:3000/api/v1/sucursal/<id>
+// deleteSucursal
 
 router.delete('/:id', async(req, res) => {
     await Sucursal.deleteOne({"_id": req.params.id});
