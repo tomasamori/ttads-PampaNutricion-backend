bec611154c5c2f486ad3df4e29c8fffd85e76eb6
Tomás Amori <tomasamori15@gmail.com>
2022-07-13 08:12:42 -0300

    fix api/v1/index.js

--- src/routes/api/v1/index.js
+++ src/routes/api/v1/index.js
@@ -1,17 +1,21 @@
 import express from 'express';
 
-import sucursal from './sucursal';
-import localidad from './localidad'
+import cliente from "./cliente";
+import empleado from "./empleado";
+import localidad from './localidad';
 import producto from './producto';
 import proveedor from "./proveedor";
+import sucursal from './sucursal';
 import tipoMascota from "./tipoMascota";
 
 const router = express.Router();
 
-router.use('/sucursal', sucursal);
+router.use('/cliente', cliente);
+router.use('/empleado', empleado);
 router.use('/localidad', localidad);
 router.use('/producto', producto);
-router.use('/proveedor',proveedor);
+router.use('/proveedor', proveedor);
+router.use('/sucursal', sucursal);
 router.use('/tipoMascota', tipoMascota);
 
 export default router;
