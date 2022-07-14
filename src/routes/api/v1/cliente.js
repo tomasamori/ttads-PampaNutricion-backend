f368359eea24bb343a7c49d40bcea7674b8ddb2e
Tomás Amori <tomasamori15@gmail.com>
2022-07-14 19:54:07 -0300

    fix api/v1/cliente.js

--- src/routes/api/v1/cliente.js
+++ src/routes/api/v1/cliente.js
@@ -3,73 +3,48 @@ import Cliente from "../../../models/Cliente";
 
 const router = express.Router();
 
-// curl -UseBasicParsing -Method GET -URI http://localhost:3000/api/v1/cliente/
 // getAll
 
 router.get('/', async (req,res)=>{
     let clientes = await Cliente.find();
-    return res.status(200).json({
-        success: true,
-            data: clientes,
-            messaga: "Lista de Clientes recuperada exitosamente"
-    })
+    return res.status(200).json(clientes);
 });
 
-// curl -UseBasicParsing -Method GET -URI http://localhost:3000/api/v1/cliente/<id>
 // getOne
 
 router.get('/:id', async(req,res)=>{
-    let cliente = await Cliente.findOne({"idUsuario":req.params.id})
-    return res.status(200).json({
-        success:true,
-        data: cliente,
-        message: "Cliente encontrado exitosamente"
-    })
+    let cli = await Cliente.findOne({"idUsuario":req.params.id});
+    return res.status(200).json(cli);
 });
 
 // getOneByUser
 
 router.get('/user/:user', async(req,res)=>{
-    let cliente = await Cliente.findOne({"usuario":req.params.user})
-    return res.status(200).json(cliente);
+    let cli = await Cliente.findOne({"usuario":req.params.user});
+    return res.status(200).json(cli);
 });
 
-// curl -UseBasicParsing -Method POST -Headers @{"Content-Type"= "application/json"} -Body '{"idUsuario": "test", "usuario": "test", "password": "test", "cuil": "test", "cuit": "test", "email": "test", "fechaNacimiento": "2000,01,01", "direccion": "test", "telefono": "test", "razonSocial": "test"}' -URI http://localhost:3000/api/v1/cliente/
 // newCliente
 
 router.post('/', async(req,res) => {
     let cli = await new Cliente(req.body);
     await cli.save();
-    return res.status(201).json({
-        success: true,
-        data: cli,
-        message: "Cliente agregado exitosamente"
-    })
+    return res.status(201).json("Cliente agregado exitosamente");
 });
 
-// curl -UseBasicParsing -Method PUT -Headers @{"Content-Type"= "application/json"} -Body '{"idUsuario": "cambio", "usuario": "cambio", "password": "cambio", "cuil": "cambio", "cuit": "cambio", "email": "cambio", "fechaNacimiento": "2001,05,26", "direccion": "cambio", "telefono": "cambio", "razonSocial": "cambio"}' -URI http://localhost:3000/api/v1/cliente/<id>
 // updateCliente
 
 router.put('/:id',async (req,res)=>{
-    let cliente = await Cliente.findOne({"idUsuario":req.params.id});
-    await cliente.update(req.body);
-    return res.status(200).json({
-        success:true,
-        data:{"idUsuario":req.params.id},
-        message: "Cliente actualizado exitosamente"
-    })
+    let cli = await Cliente.findOne({"idUsuario":req.params.id});
+    await cli.update(req.body);
+    return res.status(200).json("Cliente actualizado exitosamente");
 });
 
-// curl -UseBasicParsing -Method DELETE -URI http://localhost:3000/api/v1/cliente/<id>
 // deleteCliente
 
 router.delete('/:id', async(req, res) => {
     await Cliente.deleteOne({"idUsuario": req.params.id});
-    return res.status(200).json({
-        success: true,
-        data: {"idUsuario": req.params.id},
-        message: "Cliente eliminado exitosamente"
-    })
+    return res.status(200).json("Cliente eliminado exitosamente");
 });
 
 export default router;
