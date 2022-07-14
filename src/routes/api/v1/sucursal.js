89a1abac40511c1f8bc3d7891cea6625fd9d81ad
Tomás Amori <tomasamori15@gmail.com>
2022-07-14 18:57:29 -0300

    fix api/v1/sucursal.js

--- src/routes/api/v1/sucursal.js
+++ src/routes/api/v1/sucursal.js
@@ -1,69 +1,44 @@
+//tested
 import express from "express";
 import Sucursal from "../../../models/Sucursal";
-import Localidad from "../../../models/Localidad";
 
 const router = express.Router();
 
-// curl -UseBasicParsing -Method GET -URI http://localhost:3000/api/v1/sucursal/
 // getAll
 
 router.get('/', async(req, res) => {
     let sucursales = await Sucursal.find().populate('localidad');
-    return res.status(200).json({
-        success: true,
-        data: sucursales,
-        message: "Lista de Sucursales recuperada exitosamente"
-    })
+    return res.status(200).json(sucursales);
 });
 
-// curl -UseBasicParsing -Method GET -URI http://localhost:3000/api/v1/sucursal/<id>
 // getOne
 
 router.get('/:id', async(req, res) => {
     let suc = await Sucursal.findOne({"idSucursal": req.params.id}).populate('localidad');
-    return res.status(200).json({
-        success:true,
-        data: suc,
-        message: "Sucursal encontrada exitosamente"
-    })
+    return res.status(200).json(suc);
 });
 
-// curl -UseBasicParsing -Method POST -Headers @{"Content-Type"= "application/json"} -Body '{"nombre": "test", "direccion": "test"}' -URI http://localhost:3000/api/v1/sucursal/
 // newSucursal
 
 router.post('/', async(req, res) => {
     let suc = await new Sucursal(req.body);
     await suc.save();
-    return res.status(201).json({
-        success: true,
-        data: suc,
-        message: "Sucursal agregada exitosamente"
-    })
+    return res.status(201).json("Sucursal creada exitosamente");
 });
 
-// curl -UseBasicParsing -Method PUT -Headers @{"Content-Type"= "application/json"} -Body '{"nombre": "cambio", "direccion": "cambio"}' -URI http://localhost:3000/api/v1/sucursal/<id>
 // updateSucursal
 
 router.put('/:id', async(req, res) => {
     let suc = await Sucursal.findOne({"idSucursal": req.params.id});
     await suc.update(req.body);
-    return res.status(200).json({
-        success: true,
-        data: {"idSucursal": req.params.id},
-        message: "Sucursal actualizada exitosamente"
-    })
+    return res.status(200).json("Sucursal actualizada exitosamente");
 });
 
-// curl -UseBasicParsing -Method DELETE -URI http://localhost:3000/api/v1/sucursal/<id>
 // deleteSucursal
 
 router.delete('/:id', async(req, res) => {
     await Sucursal.deleteOne({"idSucursal": req.params.id});
-    return res.status(200).json({
-        success: true,
-        data: {"idSucursal": req.params.id},
-        message: "Sucursal eliminada exitosamente"
-    })
+    return res.status(200).json("Sucursal eliminada exitosamente");
 });
 
 export default router;
