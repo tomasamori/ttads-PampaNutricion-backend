f144697785cee8bf46e104e316c80ce9d50494b2
tomasamori <tomas.amori@gmail.com>
2024-02-20 15:41:53 -0300

    Login and register return user id

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,9 +3,9 @@ import jwt from 'jsonwebtoken'
 import config from '../config';
 import Rol from '../models/Rol';
 
-export const signUp = async(req, res, next) => {
+export const signUp = async (req, res, next) => {
 
-    const {usuario, email, password, roles, cuil, nombre, fechaNacimiento, direccion, telefono} = req.body;
+    const { usuario, email, password, roles, cuil, nombre, fechaNacimiento, direccion, telefono } = req.body;
 
     const newUsuario = new Usuario({
         usuario,
@@ -18,45 +18,46 @@ export const signUp = async(req, res, next) => {
         telefono
     });
 
-    // for security reasons, we don't want to recieve the roles from the register form
 
-    if (!roles) {
-        const rol = await Rol.findOne({name: "cliente"});
+
+    if (!roles) { // for security reasons, we don't want to recieve the roles from the register form
+        const rol = await Rol.findOne({ name: "cliente" });
         newUsuario.roles = [rol._id];
 
         const savedUsuario = await newUsuario.save();
+        const savedUsuarioId = savedUsuario._id;
 
-        const token = jwt.sign({id: savedUsuario._id},config.SECRET,{
+        const token = jwt.sign({ id: savedUsuario._id }, config.SECRET, {
             expiresIn: 86400 // 24 hours
         });
 
-        res.status(200).json({token})
+        res.status(200).json({ token, savedUsuarioId })
     }
-    else
-    {
-        res.status(404).json({message: 'No es posible asignar roles al nuevo usuario'});
+    else {
+        res.status(404).json({ message: 'No es posible asignar roles al nuevo usuario' });
         next();
         // const foundRoles = await Rol.find({name: {$in: roles}});
         // newUsuario.roles = foundRoles.map(rol => rol._id);
     }
 }
 
-export const signIn = async(req, res) => {
+export const signIn = async (req, res) => {
 
-    const usuarioFound = await Usuario.findOne({email: req.body.email}).populate('roles');
+    const usuarioFound = await Usuario.findOne({ email: req.body.email }).populate('roles');
 
     if (!usuarioFound) {
-        return res.status(400).json({message: "No se encontró el usuario"});
+        return res.status(400).json({ message: "No se encontró el usuario" });
     }
 
     const matchPassword = await Usuario.comparePassword(req.body.password, usuarioFound.password);
+    const usuarioFoundId = usuarioFound._id;
 
-    if (!matchPassword) return res.status(401).json({token: 'null', message: "Contraseña invalida"});
+    if (!matchPassword) return res.status(401).json({ token: 'null', message: "Contraseña invalida" });
 
-    const token = jwt.sign({id: usuarioFound._id}, config.SECRET, {
+    const token = jwt.sign({ id: usuarioFound._id }, config.SECRET, {
         expiresIn: 86400 // 24 hours
     })
 
-    res.json({token});
+    res.json({ token, usuarioFoundId });
 
 }
