34a80d1738885a8461243badadfce91798eb6cf4
tomasamori <tomas.amori@gmail.com>
2024-02-27 22:26:39 -0300

    Update initial setup

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 
 config();
 
-export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1/test";
+export const MONGODB_URI = process.env.NODE_ENV === 'test' ? "mongodb://127.0.0.1/test" : process.env.MONGODB_URI;
 export const PORT = process.env.PORT || 3000;
 export default {
     SECRET: 'pampa-api'
