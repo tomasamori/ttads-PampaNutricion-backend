872c051e055e4d9be5766f536dd2a9b7d04a3ed8
tomasamori <tomas.amori@gmail.com>
2024-03-05 17:38:09 -0300

    Add security methods to usuario.routes

--- src/routes/api/v1/usuario.routes.js
+++ src/routes/api/v1/usuario.routes.js
@@ -4,16 +4,16 @@ const router = Router();
 import * as usuarioCtrl from '../../../controllers/usuario.controller';
 import {authJwt, verifySignUp} from "../../../middlewares";
 
-router.get('/', usuarioCtrl.getUsuarios);
+router.get('/', [authJwt.verifyToken, authJwt.isAdmin], usuarioCtrl.getUsuarios);
 
-router.get('/:id', usuarioCtrl.getUsuarioById);
+router.get('/:id', [authJwt.verifyToken, authJwt.isAdmin], usuarioCtrl.getUsuarioById);
 
-router.get('/:id/pedidos', usuarioCtrl.findOrdersByUser);
+router.get('/pedidos/:id', [authJwt.verifyToken, authJwt.isAdmin], usuarioCtrl.findOrdersByUser);
 
-router.post('/'/*, [authJwt.verifyToken, authJwt.isAdmin, verifySignUp.checkRolesExisted, verifySignUp.checkDuplicateUsernameOrEmail]*/, usuarioCtrl.createUsuario);
+router.post('/', [authJwt.verifyToken, authJwt.isAdmin, verifySignUp.checkRolesExisted, verifySignUp.checkDuplicateUsernameOrEmail], usuarioCtrl.createUsuario);
 
-router.put('/:id'/*, [authJwt.verifyToken, authJwt.isAdmin, verifySignUp.checkRolesExisted, verifySignUp.checkDuplicateUsernameOrEmail]*/, usuarioCtrl.updateUsuarioById);
+router.put('/:id', [authJwt.verifyToken, authJwt.isAdmin, verifySignUp.checkRolesExisted, verifySignUp.checkDuplicateUsernameOrEmail], usuarioCtrl.updateUsuarioById);
 
-router.delete('/:id'/*, [authJwt.verifyToken, authJwt.isAdmin]*/,  usuarioCtrl.deleteUsuarioById);
+router.delete('/:id', [authJwt.verifyToken, authJwt.isAdmin], usuarioCtrl.deleteUsuarioById);
 
 export default router;
