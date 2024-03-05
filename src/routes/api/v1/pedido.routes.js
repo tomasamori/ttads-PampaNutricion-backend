3a3f8c983e051d7f40e2996b9cb87d1bbd5bf060
tomasamori <tomas.amori@gmail.com>
2024-03-05 17:09:17 -0300

    Add security methods to pedido.routes

--- src/routes/api/v1/pedido.routes.js
+++ src/routes/api/v1/pedido.routes.js
@@ -4,16 +4,16 @@ const router = Router();
 import * as orderCtrl from '../../../controllers/pedido.controller';
 import {authJwt} from "../../../middlewares";
 
-router.get('/', /*[authJwt.verifyToken, authJwt.isEmpleadoOrAdmin],*/ orderCtrl.getOrders);
+router.get('/', [authJwt.verifyToken, authJwt.isEmpleadoOrAdmin], orderCtrl.getOrders);
 
-router.get('/:id', /*[authJwt.verifyToken, authJwt.isEmpleado],*/ orderCtrl.getOrderById);
+router.get('/:id', [authJwt.verifyToken, authJwt.isAdmin], orderCtrl.getOrderById);
 
-router.get('/usuario/:id', orderCtrl.getOrdersByUserId);
+router.get('/usuario/:id', [authJwt.verifyToken], orderCtrl.getOrdersByUserId);
 
-router.post('/'/*, [authJwt.verifyToken, authJwt.isEmpleado]*/, orderCtrl.createOrder);
+router.post('/', [authJwt.verifyToken], orderCtrl.createOrder);
 
-router.put('/:id', /*[authJwt.verifyToken, authJwt.isEmpleadoOrAdmin],*/ orderCtrl.updateOrderById);
+router.put('/:id', [authJwt.verifyToken, authJwt.isEmpleadoOrAdmin], orderCtrl.updateOrderById);
 
-router.delete('/:id'/*, [authJwt.verifyToken, authJwt.isAdmin]*/, orderCtrl.deleteOrderById);
+router.delete('/:id', [authJwt.isAdmin], orderCtrl.deleteOrderById);
 
 export default router;
