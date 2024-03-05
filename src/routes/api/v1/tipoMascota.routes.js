7ac48342edfc76d8bbe2219b11d81a936aa48ac0
tomasamori <tomas.amori@gmail.com>
2024-03-05 17:30:01 -0300

    Add security methods to tipoMascota.routes

--- src/routes/api/v1/tipoMascota.routes.js
+++ src/routes/api/v1/tipoMascota.routes.js
@@ -2,17 +2,18 @@ import { Router } from 'express';
 const router = Router();
 
 import * as tipoMascotaCtrl from '../../../controllers/tipoMascota.controller';
+import {authJwt} from "../../../middlewares";
 
-router.get('/', tipoMascotaCtrl.getTipoMascotas);
+router.get('/', [authJwt.verifyToken, authJwt.isEmpleadoOrAdmin], tipoMascotaCtrl.getTipoMascotas);
 
-router.get('/:id', tipoMascotaCtrl.getTipoMascotaById);
+router.get('/:id', [authJwt.verifyToken, authJwt.isEmpleadoOrAdmin], tipoMascotaCtrl.getTipoMascotaById);
 
-router.get('/:id/productos', tipoMascotaCtrl.findProductoByTipoMascota);
+router.get('/productos/:id', [authJwt.verifyToken, authJwt.isAdmin], tipoMascotaCtrl.findProductoByTipoMascota);
 
-router.post('/'/*, [authJwt.verifyToken, authJwt.isEmpleado]*/, tipoMascotaCtrl.createTipoMascota);
+router.post('/', [authJwt.verifyToken, authJwt.isAdmin], tipoMascotaCtrl.createTipoMascota);
 
-router.put('/:id'/*, [authJwt.verifyToken, authJwt.isEmpleado]*/, tipoMascotaCtrl.updateTipoMascotaById);
+router.put('/:id', [authJwt.verifyToken, authJwt.isAdmin], tipoMascotaCtrl.updateTipoMascotaById);
 
-router.delete('/:id'/*, [authJwt.verifyToken, authJwt.isAdmin]*/, tipoMascotaCtrl.deleteTipoMascotaById);
+router.delete('/:id', [authJwt.verifyToken, authJwt.isAdmin], tipoMascotaCtrl.deleteTipoMascotaById);
 
 export default router;
