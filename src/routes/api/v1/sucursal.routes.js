bcce6e7545155846b153d7987e3b2bbf6d0fbe55
tomasamori <tomas.amori@gmail.com>
2024-03-05 17:27:26 -0300

    Add security methods to sucursal.routes

--- src/routes/api/v1/sucursal.routes.js
+++ src/routes/api/v1/sucursal.routes.js
@@ -2,15 +2,16 @@ import { Router } from 'express';
 const router = Router();
 
 import * as sucursalCtrl from '../../../controllers/sucursal.controller';
+import {authJwt} from "../../../middlewares";
 
 router.get('/', sucursalCtrl.getSucursales);
 
 router.get('/:id', sucursalCtrl.getSucursalById);
 
-router.post('/'/*, [authJwt.verifyToken, authJwt.isEmpleado]*/, sucursalCtrl.createSucursal);
+router.post('/', [authJwt.verifyToken, authJwt.isAdmin], sucursalCtrl.createSucursal);
 
-router.put('/:id'/*, [authJwt.verifyToken, authJwt.isEmpleado]*/, sucursalCtrl.updateSucursalById);
+router.put('/:id', [authJwt.verifyToken, authJwt.isAdmin], sucursalCtrl.updateSucursalById);
 
-router.delete('/:id'/*, [authJwt.verifyToken, authJwt.isAdmin]*/, sucursalCtrl.deleteSucursalById);
+router.delete('/:id', [authJwt.verifyToken, authJwt.isAdmin], sucursalCtrl.deleteSucursalById);
 
 export default router;
