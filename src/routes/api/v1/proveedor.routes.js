a82b892c135c65ba6784d9b65aa570bf542c85db
tomasamori <tomas.amori@gmail.com>
2024-03-05 17:21:40 -0300

    Add security methods to proveedor.routes

--- src/routes/api/v1/proveedor.routes.js
+++ src/routes/api/v1/proveedor.routes.js
@@ -2,15 +2,16 @@ import { Router } from 'express';
 const router = Router();
 
 import * as proveedorCtrl from '../../../controllers/proveedor.controller';
+import {authJwt} from "../../../middlewares";
 
-router.get('/', proveedorCtrl.getProveedores);
+router.get('/', [authJwt.verifyToken, authJwt.isAdmin], proveedorCtrl.getProveedores);
 
-router.get('/:id', proveedorCtrl.getProveedorById);
+router.get('/:id', [authJwt.verifyToken, authJwt.isAdmin], proveedorCtrl.getProveedorById);
 
-router.post('/'/*, [authJwt.verifyToken, authJwt.isEmpleado]*/, proveedorCtrl.createProveedor);
+router.post('/', [authJwt.verifyToken, authJwt.isAdmin], proveedorCtrl.createProveedor);
 
-router.put('/:id'/*, [authJwt.verifyToken, authJwt.isEmpleado]*/, proveedorCtrl.updateProveedorById);
+router.put('/:id', [authJwt.verifyToken, authJwt.isAdmin], proveedorCtrl.updateProveedorById);
 
-router.delete('/:id'/*, [authJwt.verifyToken, authJwt.isAdmin]*/, proveedorCtrl.deleteProveedorById);
+router.delete('/:id', [authJwt.verifyToken, authJwt.isAdmin], proveedorCtrl.deleteProveedorById);
 
 export default router;
