b0b2fc3ff275044ae2b9124af39cb20fa356d097
tomasamori <tomas.amori@gmail.com>
2024-03-05 16:55:32 -0300

    Add security methods to localidad.routes

--- src/routes/api/v1/localidad.routes.js
+++ src/routes/api/v1/localidad.routes.js
@@ -2,17 +2,18 @@ import { Router } from 'express';
 const router = Router();
 
 import * as localidadCtrl from '../../../controllers/localidad.controller';
+import {authJwt} from "../../../middlewares";
 
-router.get('/', localidadCtrl.getLocalidades);
+router.get('/', [authJwt.verifyToken, authJwt.isAdmin], localidadCtrl.getLocalidades);
 
-router.get('/:id', localidadCtrl.getLocalidadById);
+router.get('/:id', [authJwt.verifyToken, authJwt.isAdmin], localidadCtrl.getLocalidadById);
 
-router.get('/:id/sucursales', localidadCtrl.findSucursalByLocalidad);
+router.get('/sucursales/:id', [authJwt.verifyToken, authJwt.isAdmin], localidadCtrl.findSucursalByLocalidad);
 
-router.post('/'/*, [authJwt.verifyToken, authJwt.isEmpleado]*/, localidadCtrl.createLocalidad);
+router.post('/', [authJwt.verifyToken, authJwt.isAdmin], localidadCtrl.createLocalidad);
 
-router.put('/:id'/*, [authJwt.verifyToken, authJwt.isEmpleado]*/, localidadCtrl.updateLocalidadById);
+router.put('/:id', [authJwt.verifyToken, authJwt.isAdmin], localidadCtrl.updateLocalidadById);
 
-router.delete('/:id'/*, [authJwt.verifyToken, authJwt.isAdmin]*/, localidadCtrl.deleteLocalidadById);
+router.delete('/:id', [authJwt.verifyToken, authJwt.isAdmin], localidadCtrl.deleteLocalidadById);
 
 export default router;
