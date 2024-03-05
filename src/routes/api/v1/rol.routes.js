621ef571aaeea14871a9f8417b496d0023fa04e8
tomasamori <tomas.amori@gmail.com>
2024-03-05 17:26:09 -0300

    Add security methods to rol.routes

--- src/routes/api/v1/rol.routes.js
+++ src/routes/api/v1/rol.routes.js
@@ -2,8 +2,9 @@ import { Router } from 'express';
 const router = Router();
 
 import * as rolCtrl from '../../../controllers/rol.controller';
+import {authJwt} from "../../../middlewares";
 
-router.get('/', rolCtrl.getRoles);
+router.get('/', [authJwt.verifyToken, authJwt.isAdmin], rolCtrl.getRoles);
 
 
 export default router;
