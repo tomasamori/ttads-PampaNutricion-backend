2dea02ad7463c6aa05c703568af94ea1c92e86c4
tomasamori <tomas.amori@gmail.com>
2024-03-05 17:16:15 -0300

    Add security methods to precios.routes

--- src/routes/api/v1/precios.routes.js
+++ src/routes/api/v1/precios.routes.js
@@ -2,15 +2,16 @@ import { Router } from 'express';
 const router = Router();
 
 import * as precioCtrl from '../../../controllers/precio.controller';
+import {authJwt} from "../../../middlewares";
 
-router.get('/', precioCtrl.getPrecios);
+router.get('/', [authJwt.verifyToken, authJwt.isEmpleadoOrAdmin], precioCtrl.getPrecios);
 
-router.get('/:id', precioCtrl.getPrecioById);
+router.get('/:id', [authJwt.verifyToken, authJwt.isEmpleadoOrAdmin], precioCtrl.getPrecioById);
 
-router.post('/'/*, [authJwt.verifyToken, authJwt.isEmpleado]*/, precioCtrl.createPrecio);
+router.post('/', [authJwt.verifyToken, authJwt.isEmpleadoOrAdmin], precioCtrl.createPrecio);
 
-router.put('/:id'/*, [authJwt.verifyToken, authJwt.isEmpleado]*/, precioCtrl.updatePrecioById);
+router.put('/:id', [authJwt.verifyToken, authJwt.isAdmin], precioCtrl.updatePrecioById);
 
-router.delete('/:id'/*, [authJwt.verifyToken, authJwt.isAdmin]*/, precioCtrl.deletePrecioById);
+router.delete('/:id', [authJwt.verifyToken, authJwt.isAdmin], precioCtrl.deletePrecioById);
 
 export default router;
